cedfc17c17fbaaeda0f9a53716ff3f028ae87474
Denilson Castro <101472709+DeniCastro@users.noreply.github.com>
2022-03-27 01:52:51 -0300

    Update and rename comentários to notes.js

--- notes.js
+++ notes.js
@@ -151,4 +151,29 @@ joaozinho.calcularComissao()
 
 console.log(joaozinho)
 
+// =-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-
+// Funcões para calcular média
 
+var array = [70, 70, 80];
+array.push(20, 45, 100);
+console.log(array)
+
+console.log(mediaDoAluno(array));
+
+function mediaDoAluno(notas) {
+    const media = calcularMedia(notas);
+
+    if (media < 59) return 'F';
+    if (media < 69) return 'D';
+    if (media < 79) return 'C';
+    if (media < 89) return 'B';
+    return 'A'
+}
+
+function calcularMedia(array) {
+    let soma = 0;
+    for (let valor of array) {
+        soma += valor;
+    }
+    const media = soma / (array.length);
+}
