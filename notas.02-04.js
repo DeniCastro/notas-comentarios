5601d84e3172d7060ae50bf358f27d5414419799
Denilson Castro <101472709+DeniCastro@users.noreply.github.com>
2022-04-02 16:30:21 -0400

    Create notas.02-04.js

---
+++ notas.02-04.js
@@ -0,0 +1,36 @@
+// 
+ifpk_123@ifpk123:~$ cd ~
+ifpk_123@ifpk123:~$ mkdir deni
+mkdir: não foi possível criar o diretório “deni”: Arquivo existe
+ifpk_123@ifpk123:~$ mkdir deni
+ifpk_123@ifpk123:~$ cd deni
+ifpk_123@ifpk123:~/deni$ mkdir html
+ifpk_123@ifpk123:~/deni$ cd html
+ifpk_123@ifpk123:~/deni/html$ code .
+ifpk_123@ifpk123:~/deni/html$ ^C
+ifpk_123@ifpk123:~/deni/html$ 
+
+/* <!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <h1>Este é um título</h1>
+    <h2>Este é um subtítulo</2>
+    <h3>Este é um título nivel 3</h3>
+    <h4>Este é um título nível 4</h4>
+    <h5>este é um título nivel 5</h5>
+    <p>Este é um parágrafo</p>
+    <p>Este é outro parágrafo</p>
+    <br>
+    <p>Teste</p>
+    <p><strong>Lorem</strong> <i>ipsum dolor sit amet consectetur, adipisicing elit. Nulla deserunt eum architecto neque sed recusandae consequuntur voluptate ad magni unde!</i></p>
+    
+    
+</body>
+</html> */
+//https://www.w3schools.com/html/default.asp
